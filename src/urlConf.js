82aec36f5c9aafe45a468487a1ab9b5732ffad70
sadaf <checkok06@gmail.com>
2021-08-25 22:41:30 +0530

    FIxes bug

--- src/urlConf.js
+++ src/urlConf.js
@@ -1,3 +1,5 @@
-const baseUrl = "https://shoppingcartserver.herokuapp.com"
-// const baseUrl =  "http://localhost:2000"
+// const baseUrl = "https://shoppingcartserver.herokuapp.com"
+const baseUrl = "http://localhost:2000"
+
+
 export const url = baseUrl;
